9183ef4c66f59b204cf9c212414d93413314b7f6
ayronman99 <itechman32@gmail.com>
2026-06-24 16:47:58 +0800

    feat: add max char

--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -1,31 +1,31 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import type { ReactElement } from "react";
 import { TextField, Paper, Button } from "@mui/material";
 import useInputState from "./hooks/useInputState";
 import { DispatcherContext } from "./context/todos.context";
 import type { dispatcherHandler, TodoActionObjectType } from "./@types/todos";
+import { MAX_CHAR } from "./constant";
 
 function TodoForm(): ReactElement {
-  const [value, handleChange, reset] = useInputState("");
+  const [value, errorMsg, charCount, handleChange, validate, reset] =
+    useInputState("", MAX_CHAR);
   const dispatchTodos = useContext(
     DispatcherContext,
   ) as dispatcherHandler<TodoActionObjectType>;
-  const [isEmptyMsg, setIsEmptyMsg] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    dispatchTodos({ type: "ADD", task: value });
+    reset();
+  };
 
   return (
     <Paper>
-      <p style={{ color: "red" }}>{isEmptyMsg}</p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (value.trim() === "") {
-            setIsEmptyMsg("Todo cannot be empty");
-            return;
-          }
-          dispatchTodos({ type: "ADD", task: value });
-          reset();
-        }}
-      >
+      <p style={{ color: "red" }}>{errorMsg}</p>
+      <form onSubmit={handleSubmit}>
         <div className="flex justify-evenly gap-2 p-2">
           <TextField
             className="border-5 border-red-300"
@@ -33,17 +33,20 @@ function TodoForm(): ReactElement {
             onChange={handleChange}
             label="Add New Todo"
             fullWidth
+            inputProps={{ maxLength: MAX_CHAR }}
           />
-
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className="border-5 border-red-300"
-            disabled={!value}
-          >
-            Enter
-          </Button>
+          <div className="flex flex-col justify-between gap-2">
+            <span>{`${charCount}/${MAX_CHAR}`}</span>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className="border-5 border-red-300"
+              disabled={!value.trim()}
+            >
+              Enter
+            </Button>
+          </div>
         </div>
       </form>
     </Paper>
--- src/UpdateToDoForm.tsx
+++ src/UpdateToDoForm.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import type { ReactElement } from "react";
 import { DispatcherContext } from "./context/todos.context";
 import type { dispatcherHandler, TodoActionObjectType } from "./@types/todos";
 
@@ -7,36 +8,52 @@ import Button from "@mui/material/Button";
 import useInputState from "./hooks/useInputState";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import { MAX_CHAR } from "./constant";
 
 function UpdateToDoForm(props: {
   id: string;
   task: string;
   toggleStateEditForm: () => void;
-}) {
+}): ReactElement {
   const { id, task, toggleStateEditForm } = props;
-  const [value, handleChange, reset] = useInputState(task);
+  const [value, errorMsg, charCount, handleChange, validate, reset] =
+    useInputState(task, MAX_CHAR);
   const dispatchTodos = useContext(
     DispatcherContext,
   ) as dispatcherHandler<TodoActionObjectType>;
 
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (!validate()) {
+      return;
+    }
+    dispatchTodos({ type: "UPDATE", id: id, newTask: value });
+    reset();
+    toggleStateEditForm();
+  };
+
   return (
     <form
-      onSubmit={(evt) => {
-        evt.preventDefault();
-        dispatchTodos({ type: "UPDATE", id: id, newTask: value });
-        reset();
-        toggleStateEditForm();
-      }}
+      onSubmit={handleSubmit}
       style={{ width: "100%" }}
       className="is-flex is-justify-content-space-between"
     >
-      <TextField
-        margin="normal"
-        value={value}
-        onChange={handleChange}
-        fullWidth
-      />
-      <Button type="submit">
+      <div style={{ width: "100%" }}>
+        <TextField
+          margin="normal"
+          value={value}
+          onChange={handleChange}
+          fullWidth
+          inputProps={{ maxLength: MAX_CHAR }}
+        />
+        <div className="is-flex is-justify-content-space-between">
+          <span style={{ color: errorMsg ? "red" : "inherit" }}>
+            {errorMsg}
+          </span>
+          <span>{`${charCount}/${MAX_CHAR}`}</span>
+        </div>
+      </div>
+      <Button type="submit" disabled={!value.trim()}>
         <FontAwesomeIcon icon={faFloppyDisk} className="is-size-4" />
       </Button>
     </form>
